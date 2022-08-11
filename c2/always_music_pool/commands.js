2f3d65b39745d83ec3912e85ed96d7484e1c2cdd
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-08-11 13:07:39 -0400

    Manejo de errores con try catch

--- c2/always_music_pool/commands.js
+++ c2/always_music_pool/commands.js
@@ -5,53 +5,77 @@ const pool = new Pool(config);
 
 async function getStudents() {
     const client = await pool.connect();
-    const resp = await client.query('select * from students order by name_student');
-    console.log(resp.rows);
+    try {
+        const resp = await client.query('select * from students order by name_student');
+        console.log(resp.rows);
+    } catch (error) {
+        console.log("Error en la consulta: " + error);
+    }
     client.release();
     pool.end();
 }
 
 async function newStudent(name, rut, grade, level) {
     const client = await pool.connect();
-    const resp = await client.query(`insert into students (name_student, rut, grade, level) values ('${name}', '${rut}', '${grade}', ${level}) returning *`)
+    try {
+        const resp = await client.query(`insert into students (name_student, rut, grade, level) values ('${name}', '${rut}', '${grade}', ${level}) returning *`)
+    } catch (error) {
+        console.log("Error en la consulta: " + error);
+    }
     client.release();
     pool.end();
 }
 
 async function getStudentByRut(rut) {
     const client = await pool.connect();
-    const resp = await client.query(`select * from students where rut='${rut}'`);
-    console.log(resp.rows);
+    try {
+        const resp = await client.query(`select * from students where rut='${rut}'`);
+        console.log(resp.rows);
+    } catch (error) {
+        console.log("Error en la consulta: " + error);
+    }
     client.release();
     pool.end();
 }
 
 async function updateStudent(name, grade, level, rut) {
     const client = await pool.connect();
-    const resp = await client.query(`select * from students where rut='${rut}'`);
-    let nameStudent = resp.rows[0].name_student;
-    const respUpdate = await client.query(`update students set name_student ='${name}', grade='${grade}', level=${level} where rut='${rut}'`);
-    console.log(`El/La estudiante ${nameStudent} se ha editado con éxito`);
+    try {
+        const resp = await client.query(`select * from students where rut='${rut}'`);
+        let nameStudent = resp.rows[0].name_student;
+        const respUpdate = await client.query(`update students set name_student ='${name}', grade='${grade}', level=${level} where rut='${rut}'`);
+        console.log(`El/La estudiante ${nameStudent} se ha editado con éxito`);
+    } catch (error) {
+        console.log("Error en la consulta: " + error);
+    }
     client.release();
     pool.end();
 }
 
 async function deleteStudent(rut) {
     const client = await pool.connect();
-    const resp = await client.query(`select * from students where rut='${rut}'`);
-    let nameStudent = resp.rows[0].name_student;
-    const respDelete = await client.query(`delete from students where rut='${rut}'`);
-    console.log(`El/La estudiante ${nameStudent} se eliminó con éxito`);
+    try {
+        const resp = await client.query(`select * from students where rut='${rut}'`);
+        let nameStudent = resp.rows[0].name_student;
+        const respDelete = await client.query(`delete from students where rut='${rut}'`);
+        console.log(`El/La estudiante ${nameStudent} se eliminó con éxito`);
+    } catch (error) {
+        console.log("Error en la consulta: " + error);
+    }
     client.release();
     pool.end();
 }
 
 async function approveStudent(rut) {
     const client = await pool.connect();
-    const respLevel = await client.query(`select * from students where rut='${rut}'`);
-    let level = respLevel.rows[0].level;
-    level++;
-    const resp = await client.query(`update students set level=${level} where rut='${rut}'`);
+    try {
+        const respLevel = await client.query(`select * from students where rut='${rut}'`);
+        let level = respLevel.rows[0].level;
+        level++;
+        const resp = await client.query(`update students set level=${level} where rut='${rut}'`);
+    } catch (error) {
+        console.log("Error en la consulta: " + error);
+    }
     client.release();
     pool.end();
 }
@@ -69,18 +93,10 @@ if (action == 'mostrar') {
         grade: process.argv[5].trim(),
         level: process.argv[6].trim()
     }
-    try {
-        newStudent(student.name, student.rut, student.grade, student.level);
-    } catch (error) {
-        console.log("Surgió un error: " + erro);
-    }
+    newStudent(student.name, student.rut, student.grade, student.level);
 } else if (action == 'consulta_rut') {
     const rut = process.argv[3];
-    try {
-        getStudentByRut(rut);
-    } catch (error) {
-        console.log("Surgió un error: " + error);
-    }
+    getStudentByRut(rut);
 } else if (action == 'editar') {
     let student = {
         name: process.argv[3].trim(),
@@ -88,25 +104,13 @@ if (action == 'mostrar') {
         level: process.argv[5].trim(),
         rut: process.argv[6].trim()
     }
-    try {
-        updateStudent(student.name, student.grade, student.level, student.rut);
-    } catch (error) {
-        console.log("Surgió un error: " + error);
-    }
+    updateStudent(student.name, student.grade, student.level, student.rut);
 } else if (action == 'eliminar') {
     const rut = process.argv[3];
-    try {
-        deleteStudent(rut);
-    } catch (error) {
-        console.log("Surgió un error: " + error);
-    }
+    deleteStudent(rut);
 } else if (action == 'aprobar') {
     const rut = process.argv[3].trim();
-    try {
-        approveStudent(rut);
-    } catch (error) {
-        console.log("Surgió un error: " + error);
-    }
+    approveStudent(rut);
 } else {
     console.log(`La acción ${action} no está implementa`);
 }
